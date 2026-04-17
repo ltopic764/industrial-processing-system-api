93a203fdc171f0c73ce43d52e4a6796ad3b019f0
ltopic764 <ltopic764@gmail.com>
2026-04-17 11:44:13 +0200

    Implement Submit function logic
    Keep track of the completed jobs as one job cannot be completed more than once, also track the jobs priority when it comes to processing.

--- IndustrialProcessingSystem/IndustrialProcessingSystem/services/ProcessingSystem.cs
+++ IndustrialProcessingSystem/IndustrialProcessingSystem/services/ProcessingSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IndustrialProcessingSystem.config;
+using IndustrialProcessingSystem.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,39 @@ using System.Threading.Tasks;
 
 namespace IndustrialProcessingSystem.services
 {
-    internal class ProcessingSystem
+    public class ProcessingSystem
     {
+        private readonly PriorityQueue<Job, int> _queue = new();
+        private readonly HashSet<Guid> _submittedIds = new();
+        private readonly int _maxQueueSize;
+        private readonly object _lock = new();
+
+        public ProcessingSystem(SystemConfig config)
+        {
+            _maxQueueSize = config.MaxQueueSize;
+        }
+
+        public JobHandle? Submit(Job job)
+        {
+            lock(_lock)
+            {
+                // Ne sme isti posao da se obavi vise puta
+                if (_submittedIds.Contains(job.Id))
+                {
+                    return null;
+                }
+
+                // Red je pun, odbicemo posao
+                if (_queue.Count >= _maxQueueSize)
+                {
+                    return null;
+                }
+
+                var handle = new JobHandle(job.Id);
+                _queue.Enqueue(job, job.Priority);
+                _submittedIds.Add(job.Id);
+                return handle;
+            }
+        }
     }
 }
