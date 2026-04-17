6444eaf7942ae82e248f438509d0b89d9786fbd5
ltopic764 <ltopic764@gmail.com>
2026-04-17 11:22:34 +0200

    Implemented XML parsing

---
+++ IndustrialProcessingSystem/IndustrialProcessingSystem/config/SystemConfig.cs
@@ -0,0 +1,32 @@
+﻿using IndustrialProcessingSystem.enums;
+using IndustrialProcessingSystem.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IndustrialProcessingSystem.config
+{
+    public class SystemConfig
+    {
+        public int WorkerCount { get; set; }
+        public int MaxQueueSize { get; set; }
+        public List<Job> Jobs { get; set; } = new();
+
+        public static SystemConfig LoadFromXml(string path)
+        {
+            var config = new SystemConfig();
+            XElement xmlData = XElement.Load(path);
+
+            config.WorkerCount = int.Parse(xmlData.Element("WorkerCount").Value);
+            config.MaxQueueSize = int.Parse(xmlData.Element("MaxQueueSize").Value);
+
+            config.Jobs = (from jobEl in xmlData.Descendants("Job")
+                           select new Job
+                           (Guid.NewGuid(), Enum.Parse<JobType>(jobEl.Attribute("Type").Value), jobEl.Attribute("Payload").Value, int.Parse(jobEl.Attribute("Priority").Value))).ToList();
+            return config;
+        }
+    }
+}
--- IndustrialProcessingSystem/IndustrialProcessingSystem/models/JobHandle.cs
+++ IndustrialProcessingSystem/IndustrialProcessingSystem/models/JobHandle.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace IndustrialProcessingSystem.models
 {
-    internal class JobHandle
+    public class JobHandle
     {
         public Guid Id { get; set; }
-        public Task<int> Result { get; set; }
+        public Task<int> Result => _tsc.Task;
+
+        private readonly TaskCompletionSource<int> _tsc = new();
+
+        public JobHandle(Guid id) { Id = id; }
+
+        public void Complete(int result) => _tsc.SetResult(result);
+        public void Fail(Exception ex) => _tsc.SetException(ex);
     }
 }
